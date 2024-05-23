826b61a3b819e821ecea9824c75bf2bc37da3676
Martin SCAURI <131989712+Scaruu@users.noreply.github.com>
2024-05-23 15:17:22 +0200

    Retrait de Jquery
    Suppression de la dépendance jQuery pour réduire le temps de chargement de la bibliothèque

--- js/chat-widget.js
+++ js/chat-widget.js
@@ -1,5 +1,15 @@
-jQuery(document).ready(function ($) {
-  $("#chat-widget-icon").on("click", function () {
-    $("#chat-widget-modal").toggle();
+document.addEventListener("DOMContentLoaded", function () {
+  var chatWidgetIcon = document.getElementById("chat-widget-icon");
+  var chatWidgetModal = document.getElementById("chat-widget-modal");
+
+  chatWidgetIcon.addEventListener("click", function () {
+    if (
+      chatWidgetModal.style.display === "none" ||
+      chatWidgetModal.style.display === ""
+    ) {
+      chatWidgetModal.style.display = "block";
+    } else {
+      chatWidgetModal.style.display = "none";
+    }
   });
 });

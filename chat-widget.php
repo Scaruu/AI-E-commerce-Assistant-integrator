1a4b8fcf9feadd065e812ca055dc3e62b9b9738b
Martin SCAURI <131989712+Scaruu@users.noreply.github.com>
2024-05-23 15:20:38 +0200

    Amélioration php du plugin
    Uniformisation des versions CSS/JS. Amélioration visibilité.

--- chat-widget.php
+++ chat-widget.php
@@ -11,17 +11,20 @@ if (!defined('ABSPATH')) {
 }
 
 function chat_widget_enqueue_scripts() {
-    $css_version = '1.1.3';
-    $js_version = '1.0.1';
+    $version = '1.0.0';
     
-    wp_enqueue_style('chat-widget-style', plugin_dir_url(__FILE__) . 'css/chat-widget.css', array(), $css_version);
-    wp_enqueue_script('chat-widget-script', plugin_dir_url(__FILE__) . 'js/chat-widget.js', array('jquery'), $js_version, true);
+    wp_enqueue_style('chat-widget-style', plugin_dir_url(__FILE__) . 'css/chat-widget.css', array(), $version);
+    wp_enqueue_script('chat-widget-script', plugin_dir_url(__FILE__) . 'js/chat-widget.js', array('jquery'), $version, true);
 }
 add_action('wp_enqueue_scripts', 'chat_widget_enqueue_scripts');
 
 function chat_widget_add_icon() {
-    echo '<div id="chat-widget-icon" class="chat-widget-icon"></div>';
-    echo '<div id="chat-widget-modal" class="chat-widget-modal"><iframe src="https://iframechatms.netlify.app/" width="100%" height="100%" frameborder="0"></iframe></div>';
+    ?>
+    <div id="chat-widget-icon" class="chat-widget-icon"></div>
+    <div id="chat-widget-modal" class="chat-widget-modal">
+        <iframe src="https://iframechatms.netlify.app/" width="100%" height="100%" frameborder="0"></iframe>
+    </div>
+    <?php
 }
 add_action('wp_footer', 'chat_widget_add_icon');
 ?>
